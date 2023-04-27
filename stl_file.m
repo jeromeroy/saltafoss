92097357898732623ecbecb06b5dfac65d337a38
jeromeroy <j.roy@hotmail.ch>
2023-04-27 11:54:12 +0200

    divide the output stl file in 12 subfiles

--- stl_file.m
+++ stl_file.m
@@ -1,5 +1,5 @@
 %% Saltafoss
-% Desciption:   This code exports audio files in STL files using 3D graphs
+% Desciption:   This code exports audio files into STL files using 3D graphs
 % Author:       Jérôme Roy
 % Date:         10.04.23
 % Update:       25.04.23
@@ -22,21 +22,38 @@ for i = 1:length(file_list)
     [p,f,t] = pspectrum(y,Fs,'spectrogram');
     a = sqrt(p.*f*3);
     
-    % calculate scaling factor to make frequency and time axes roughly equal
+    % calculate scaling factor to print in 2/3 ratio (20x30cm)
     x = 2/3*(f(end)/t(end));
     
     % select frequency range and compress data
-    ff = transpose(f(1:800,:))./x;
-    aa = a(1:800,:)*400;
-    c = 2; % how much should i compress?
+    ff = transpose(f(1:800,:))./x; 
+    aa = a(1:800,:)*200;
+    c = 10; % how much should i "compress" the mesh?
     F = ff(:,1:c:end);
     A = aa(1:c:end,1:c:end);
-    
+
     % create time axis
     T = transpose(linspace(0, length(y)/Fs, size(A,2)));
     
-    % write ascii STL file
-    stl_filename = [filename(1:end-4) '.stl'];
-    stlwrite(fullfile(folder_path, stl_filename),T,F,A,'mode','ascii')
+    % divide the area in 12 tiles, arranged in a 4x3 matrix
+
+    length_F = length(F)/4;
+    length_T = length(T)/3;
+    input_text = "piastrella"; % Replace with your desired text input
+
+    for i = 1:4
+        for g = 1:3
+            tic;
+            
+            X = F(:,length_F*(i-1)+1:1:length_F*i-1);
+            Y = T(length_T*(g-1)+1:1:length_T*g-1,:);
+            Z = A(length_F*(i-1)+1:1:length_F*i-1,length_T*(g-1)+1:1:length_T*g-1);
+
+            stl_filename = sprintf('%s_%d_%d.stl', input_text, i, g);
+            stlwrite(fullfile(folder_path, stl_filename),Y,X,Z,'mode','ascii')
+            toc;
+        end
+    end
+
 end
 toc;
